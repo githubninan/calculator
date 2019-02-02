81b4b40ce3108be10c81667f9beff8144744bb7d
nn <ninain@gmail.com>
2019-02-02 00:42:28 -0800

    create basic html, js files for add, substract, multiply, divide mathematical operations.

---
+++ js/scipts.js
@@ -0,0 +1,31 @@
+var add = function (number1, number2) {
+return number1 + number2;
+};
+var number1 = parseInt(prompt ("Enter a number"));
+var number2 = parseInt(prompt ("Enter another number"));
+var result = add (number1, number2);
+alert(result);
+
+var number1 = parseInt(prompt ("Enter a number"));
+var number2 = parseInt(prompt ("Enter another number"));
+var substract = function(number1, number2) {
+return number1-number2;
+};
+var result=substract (number1, number2);
+alert (result);
+
+var number1 = parseInt(prompt ("Enter a number"));
+var number2 = parseInt(prompt ("Enter another number"));
+var multiply = function (number1, number2) {
+  return number1*number2;
+};
+var result = multiply (number1, number2);
+alert (result);
+
+var number1 = parseInt(prompt ("Enter a number"));
+var number2 = parseInt(prompt ("Enter another number"));
+var divide= function (number1, number2) {
+  return number1/number2;
+};
+var result = divide (number1, number2);
+alert(result);
